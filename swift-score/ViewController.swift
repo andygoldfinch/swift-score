6f2e4ea6e84f357ff85f1bcc514f922cf87bb746
Andy Goldfinch <andygoldfinch@gmail.com>
2017-02-13 14:45:58 +0000

    Initial Commit

---
+++ swift-score/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-score
+//
+//  Created by Andy Goldfinch on 13/02/2017.
+//  Copyright © 2017 Andy Goldfinch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
