c3e839cad22875e0eeac3f5872c5af168aa38d55
Andy Goldfinch <andygoldfinch@gmail.com>
2017-02-13 16:41:44 +0000

    Setup Cocoa Pods

---
+++ Pods/Target Support Files/Pods-swift-score/Pods-swift-score-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_swift_score : NSObject
+@end
+@implementation PodsDummy_Pods_swift_score
+@end
---
+++ swift-score/Parser.swift
@@ -0,0 +1,9 @@
+//
+//  Parser.swift
+//  swift-score
+//
+//  Created by Andy Goldfinch on 13/02/2017.
+//  Copyright © 2017 Andy Goldfinch. All rights reserved.
+//
+
+import Foundation
